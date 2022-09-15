340d0d3f7868be12e9cb4ebf7e46a86bde635c78
emanishimwe <manishimweemmanuel8@gmail.com>
2022-09-15 21:29:59 +0200

    feat: add customer user model

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -7,3 +7,7 @@ from django.urls import path,include
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header='PETR Estate admin'
+admin.site.site_title='PETR Real Estate Admin Portal'
+admin.site.index_title='Welcome to the PETR Real Estate Protal'
