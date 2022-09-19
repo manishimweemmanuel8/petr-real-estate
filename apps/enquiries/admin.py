cadd6c241fc1949c4490fcf8b667c0bbc1c4d4a6
emanishimwe <manishimweemmanuel8@gmail.com>
2022-09-19 11:03:42 +0200

    feat: add ratings and enquiries view

--- apps/enquiries/admin.py
+++ apps/enquiries/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .serializers import Enquiry
+from .models import Enquiry
 
 
 class EnquiryAdmin(admin.ModelAdmin):
--- apps/enquiries/serializers.py
+++ apps/enquiries/serializers.py
@@ -1,21 +1,9 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from phonenumber_field.modelfields import PhoneNumberField
+from rest_framework import serializers
 
-from apps.common.models import TimeStampedUUIDModel
+from .models import Enquiry
 
 
-class Enquiry(TimeStampedUUIDModel):
-    name = models.CharField(_("Your Name"), max_length=100)
-    phone_number = PhoneNumberField(
-        _("Phone number"), max_length=30, default="+250784123456"
-    )
-    email = models.EmailField(_("Email"))
-    subject = models.CharField(_("Subject"), max_length=100)
-    message = models.TextField(_("Message"))
-
-    def __str__(self):
-        return self.email
-
+class EnquirySerializer(serializers.ModelSerializer):
     class Meta:
-        verbose_name_plural = "Enquiries"
+        model = Enquiry
+        fields = "__all__"
